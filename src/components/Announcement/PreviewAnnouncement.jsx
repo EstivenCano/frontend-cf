bf0c272a7532117090b9b0c1023b1dd0b01ffca2
EstivenCano <estivencano99@gmail.com>
2021-03-19 21:55:37 -0500

    Previsualización de convocatoria

--- src/components/Announcement/PreviewAnnouncement.jsx
+++ src/components/Announcement/PreviewAnnouncement.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AnnouncementContext } from "./AnnouncementContext";
 import {
   Image,
@@ -12,21 +12,23 @@ import {
   Segment,
   Icon,
   Label,
+  LabelDetail,
 } from "semantic-ui-react";
 import "./PreviewAnnouncement.css";
 
 const PreviewAnnouncement = () => {
-  const {value} = useContext(AnnouncementContext)
+  const { value } = useContext(AnnouncementContext);
   const [announcement] = value;
+  const [visible, setVisible] = useState(false);
 
   return (
     <Segment stacked style={{ minWidth: 600 }}>
       <Grid columns={2}>
         <GridRow stretched>
-          <GridColumn width={9}>
+          <GridColumn width={9} verticalAlign="middle">
             <GridRow>
               <Image
-                src="https://react.semantic-ui.com/images/wireframe/image-text.png"
+                src="https://udem.edu.co/images/ADMISIONES/Imagenes/flexible.jpg"
                 as="a"
                 size="medium"
                 href="/start"
@@ -64,14 +66,26 @@ const PreviewAnnouncement = () => {
               <Header as="h4" color="teal" attached="top">
                 <strong>Pregrado: </strong> {announcement.pregrado}
               </Header>
-              <Header as="h4" color="grey" textAlign="justified" attached style={{ maxHeight: 215 }}>
+              <Header
+                as="h4"
+                color="grey"
+                textAlign="justified"
+                attached
+                style={{ maxHeight: 215 }}
+              >
                 {announcement.descripcion}
               </Header>
             </GridRow>
           </GridColumn>
           <GridColumn width="16">
             <Divider />
-            <Button animated='vertical'>
+            <Button
+              animated="vertical"
+              color="blue"
+              onClick={() => {
+                setVisible(visible === true ? false : true);
+              }}
+            >
               <ButtonContent hidden>Ver más</ButtonContent>
               <Button.Content visible>
                 <Icon name="arrow alternate circle down" />
@@ -79,6 +93,108 @@ const PreviewAnnouncement = () => {
             </Button>
           </GridColumn>
         </GridRow>
+        {visible ? (
+          <>
+            <GridRow centered>
+              <Header as="h5" color="grey" textAlign="center">
+                Recuerda revisar los cupos y el horario de las materias antes de
+                aplicar.
+              </Header>
+            </GridRow>
+            <Divider />
+            <GridRow centered className="row-materias">
+              {announcement.cursos.map((curso) => {
+                return (
+                  <>
+                    <GridColumn width={6} verticalAlign="middle">
+                      <Header as="h4" color="blue" textAlign="center">
+                        <Icon name="book" />
+                        {curso.curso}
+                      </Header>
+                    </GridColumn>
+                    <GridColumn width={1}>
+                      <Divider vertical>
+                        <Icon name="long arrow alternate right" />
+                      </Divider>
+                    </GridColumn>
+                    <GridColumn width={9}>
+                      <Header as="h4" color="red" textAlign="center">
+                        <Icon name="group" />
+                        {"Grupos"}
+                      </Header>
+                      <GridRow>
+                        {curso.grupos !== undefined ? curso.grupos.map((grupo) => {
+                          return (
+                            <>
+                              <GridRow>
+                                <Header
+                                  textAlign="center"
+                                  attached="top"
+                                  className="header-grupo"
+                                  color="grey"
+                                >
+                                  <GridColumn>
+                                    {"Grupo:" + grupo.grupo}
+                                  </GridColumn>
+                                  <GridColumn>
+                                    {"Cupos:" + grupo.cupos}
+                                  </GridColumn>
+                                </Header>
+                              </GridRow>
+                              <GridColumn>
+                                <Header
+                                  textAlign="center"
+                                  attached
+                                >
+                                  <Label as="a" color="grey" image>
+                                    <Icon name="calendar" />
+                                    Dias
+                                    <LabelDetail>
+                                      {grupo.horario !== undefined ? grupo.horario.dias.toString() : ""}
+                                    </LabelDetail>
+                                  </Label>
+
+                                  <Grid style={{ marginTop: 0 }}>
+                                    <GridColumn textAlign="center">
+                                      <Label as="a" color="blue" image>
+                                        <Icon name="clock" />
+                                        Inicio
+                                        <Label.Detail>
+                                          {grupo.horario !== undefined
+                                            ? grupo.horario.h_inicio
+                                                .toString()
+                                                .slice(15, 25)
+                                            : ""}
+                                        </Label.Detail>
+                                      </Label>
+                                      <Label as="a" color="red" image>
+                                        <Icon name="clock" /> Fin{" "}
+                                        <Label.Detail>
+                                          {grupo.horario !== undefined
+                                            ? grupo.horario.h_fin
+                                                .toString()
+                                                .slice(15, 25)
+                                            : ""}
+                                        </Label.Detail>
+                                      </Label>
+                                    </GridColumn>
+                                  </Grid>
+                                </Header>
+                              </GridColumn>
+                              <Divider/>
+                            </>
+                          );
+                        }) : <></>}
+                      </GridRow>
+                    </GridColumn>
+                  </>
+                );
+              })}
+            </GridRow>
+          </>
+        ) : (
+          <></>
+        )}
       </Grid>
     </Segment>
   );
